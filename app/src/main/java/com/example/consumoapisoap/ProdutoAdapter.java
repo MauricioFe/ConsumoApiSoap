4ba959793e3daf1177e52c28be74808a24bae5c3
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-20 13:53:36 -0300

    Criando Adapter de produto

--- app/src/main/java/com/example/consumoapisoap/ProdutoAdapter.java
+++ app/src/main/java/com/example/consumoapisoap/ProdutoAdapter.java
@@ -1,9 +1,14 @@
 package com.example.consumoapisoap;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityManager;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.consumoapisoap.models.Produto;
 
@@ -20,21 +25,43 @@ public class ProdutoAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return produtoList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return produtoList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return produtoList.get(position).getId();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        convertView = LayoutInflater.from(context).inflate(R.layout.item_list_produto, parent, false);
+
+        TextView txtNome = convertView.findViewById(R.id.item_list_txt_nome);
+        TextView txtPreco = convertView.findViewById(R.id.item_list_txt_preco);
+        ImageView imgDelete = convertView.findViewById(R.id.item_list_btn_delete);
+        ImageView imgEdit = convertView.findViewById(R.id.item_list_btn_edit);
+
+        Produto produto = (Produto) getItem(position);
+        txtNome.setText(produto.getNome());
+        txtPreco.setText(String.valueOf(produto.getPreco()));
+        imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "deletando", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imgEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Editando", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return convertView;
     }
 }
